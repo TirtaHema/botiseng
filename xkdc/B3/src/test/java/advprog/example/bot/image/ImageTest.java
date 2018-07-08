f6e6dd4e9b3e11b42b78b601ae1de32001e1aedd
Tirta Hema Jaya Hidayat <tirta.hema@ui.ac.id>
2018-07-08 22:44:47 +0700

    initial commit

---
+++ xkdc/B3/src/test/java/advprog/example/bot/image/ImageTest.java
@@ -0,0 +1,26 @@
+package advprog.example.bot.image;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class hot100Test {
+
+    private image image;
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void Test() {
+
+    }
+
+
+}
